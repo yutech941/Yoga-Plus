ce653e3de86aebc4b9fbd91b964d3ce62eedee4d
小林優一 <y.k94cofitec179@gmail.com>
2020-01-10 16:20:37 +0900

    完成

--- js/app.js
+++ js/app.js
@@ -2,9 +2,19 @@ $(function() {
 
     // フロートヘッダーメニュー
     var targetHeight = $('.js-float-menu-target').height();
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
     });
-});
 
 // spメニュー
+    $('.js-toggle-sp-menu').on('click', function () {
+        $(this).toggleClass('active');
+        $('.js-toggle-sp-menu-target').toggleClass('active');
+    });
+
+//メニューを押して閉じる
+    $('.js-click-close-menu').on('click', function () {
+        $('.js-toggle-sp-menu').removeClass('active');
+        $('.js-toggle-sp-menu-target').toggleClass('active');
+    });
+});
